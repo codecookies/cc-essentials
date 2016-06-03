5c4b28a6ccc83535b677c80bcd50beb297625b7f
codecookies <jal@Jals-iMac.local>
2016-06-03 15:22:35 +0530

    UX improvements

--- assets/js/cce-loveit.js
+++ assets/js/cce-loveit.js
@@ -7,7 +7,11 @@ jQuery(document).ready(function($){
 			link.addClass('loved');
 			
     		var id = $(this).attr('id'),
-    			suffix = link.find('.cce-loveit-suffix').text();
+    			suffix = link.find('.cce-loveit-suffix').text(),
+    			count = parseInt(link.find('.cce-loveit-count').text());
+    		
+    		link.find('.cce-loveit-count').text(count+1);	// User Experience: Immediately acknowledging the user that your ‘love’ has been 
+    														// received while we send the request to the server and wait for the response.
 			
     		$.post(cce_loveit.ajaxurl, { action:'cce_loveit', loves_id:id, suffix:suffix }, function(data){
     			link.html(data).addClass('loved').attr('title',cce_loveit.loved_text);
