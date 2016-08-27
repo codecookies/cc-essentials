d6d9a9378a38d0f2eff226e43982c3d8d7b90127
codecookies <jal@Jals-iMac.local>
2016-08-27 23:33:00 +0530

    Visual enhancements

--- loveit/cce-loveit.php
+++ loveit/cce-loveit.php
@@ -8,7 +8,7 @@ class CCELoveIt {
 	    $cce_options = get_option('cce_options');
 	    $show_on_posts = isset($cce_options['show_loveit_button_on']['post']) ? $cce_options['show_loveit_button_on']['post'] : FALSE;
 	    $show_on_pages = isset($cce_options['show_loveit_button_on']['page']) ? $cce_options['show_loveit_button_on']['page'] : FALSE;
-	    
+
         add_action('wp_enqueue_scripts', array(&$this, 'enqueue_scripts'));
         add_action('admin_enqueue_scripts', array( &$this, 'admin_enqueue_scripts') );
         add_filter('the_content', array(&$this, 'the_content'));
@@ -27,65 +27,65 @@ class CCELoveIt {
         add_shortcode('cce_loveit', array(&$this, 'shortcode'));
         add_action('widgets_init', create_function('', 'register_widget("CCELoveIt_Widget");'));
 	}
-	
-	function enqueue_scripts() {	
-		
+
+	function enqueue_scripts() {
+
 		$cce_options = get_option('cce_options');
-		
+
 		if ( !empty($cce_options['show_loveit_button_on']) ) {
-		
+
 			global $cce;
 			wp_enqueue_style( 'cce-loveit', $cce->plugin_url() . '/assets/css/cce-loveit.css' );
 			wp_enqueue_script( 'cce-loveit', $cce->plugin_url() . '/assets/js/min/cce-loveit.min.js', array('jquery'), false, true );
 			wp_localize_script( 'cce-loveit', 'cce_loveit', array('ajaxurl' => admin_url('admin-ajax.php'), 'loved_text' => __( 'You already loved this!', 'cc-essentials' )) );
 		}
 	}
-	
+
 	function admin_enqueue_scripts() {
-		
+
 		$cce_options = get_option('cce_options');
-		
+
 		if ( !empty($cce_options['show_loveit_button_on']) ) {
-			
+
 			global $cce;
 			wp_enqueue_style( 'cce-admin-loveit-styles', $cce->plugin_url() . '/assets/css/cce-admin-styles-loveit.css' );
 		}
 	}
-	
-	function the_content( $content ) {		
+
+	function the_content( $content ) {
 	   // Don't show on custom page templates
 	    if(is_page_template()) return $content;
 	    // Don't show on Stacked slides
 	    if(get_post_type() == 'slide') return $content;
-	    
+
 		global $wp_current_filter;
 		if ( in_array( 'get_the_excerpt', (array) $wp_current_filter ) ) {
 			return $content;
 		}
-		
+
 		$cce_options = get_option('cce_options');
 		$show_on_posts = isset($cce_options['show_loveit_button_on']['post']) ? $cce_options['show_loveit_button_on']['post'] : FALSE;
 	    $show_on_pages = isset($cce_options['show_loveit_button_on']['page']) ? $cce_options['show_loveit_button_on']['page'] : FALSE;
-		
+
 		if( is_singular('post') && $show_on_posts ) $content .= $this->do_likes('loveit-after-content');
 		if( is_page() && !is_front_page() && $show_on_pages ) $content .= $this->do_likes('loveit-after-content');
-		
+
 		//Under consideration: if(( is_front_page() || is_home() || is_category() || is_tag() || is_author() || is_date() || is_search()) && $options['add_to_other'] ) $content .= $this->do_likes();
-		
+
 		return $content;
 	}
-	
+
 	function setup_loveit( $post_id ) {
 		if(!is_numeric($post_id)) return;
-	
+
 		add_post_meta($post_id, '_cce_loves', '0', true);
 	}
-	
+
 	function cce_loveit_column_title( $defaults ) {
 		$defaults['cce-loveit-data'] = __( 'Loves received', 'cc-essentials' );
 		return $defaults;
 	}
-	
+
 	function cce_loveit_column_content( $column_name, $post_id ) {
 		if ($column_name == 'cce-loveit-data') {
 			$loves = get_post_meta($post_id, '_cce_loves', true);
@@ -96,7 +96,7 @@ class CCELoveIt {
 			}
 		}
 	}
-	
+
 	function ajax_callback( $post_id ) {
 
 		$cce_options = get_option('cce_options');
@@ -110,65 +110,65 @@ class CCELoveIt {
 			$post_id = str_replace('cce-loveit-', '', $_POST['post_id']);
 			echo $this->cce_love_this($post_id, $cce_options['suffix_text_zero'], $cce_options['suffix_text_one'], $cce_options['suffix_text_more'], 'get');
 		}
-		
+
 		exit;
 	}
-	
+
 	function cce_love_this($post_id, $suffix_text_zero = false, $suffix_text_one = false, $suffix_text_more = false, $action = 'get') {
 		if(!is_numeric($post_id)) return;
 		$suffix_text_zero = strip_tags($suffix_text_zero);
 		$suffix_text_one = strip_tags($suffix_text_one);
-		$suffix_text_more = strip_tags($suffix_text_more);		
-		
+		$suffix_text_more = strip_tags($suffix_text_more);
+
 		switch($action) {
-		
+
 			case 'get':
 				$loves = get_post_meta($post_id, '_cce_loves', true);
 				if( !$loves ){
 					$loves = 0;
 					add_post_meta($post_id, '_cce_loves', $loves, true);
 				}
-				
+
 				if( $loves == 0 ) { $suffix = $suffix_text_zero; }
 				elseif( $loves == 1 ) { $suffix = $suffix_text_one; }
 				else { $suffix = $suffix_text_more; }
-				
+
 				return '<span class="cce-loveit-count">'. $loves .'</span><span class="cce-loveit-suffix">'. $suffix .'</span>';
 				break;
-				
+
 			case 'update':
 				$loves = get_post_meta($post_id, '_cce_loves', true);
 				if( isset($_COOKIE['cce_loves_'. $post_id]) ) return $loves;
-				
+
 				$loves++;
 				update_post_meta($post_id, '_cce_loves', $loves);
 				setcookie('cce_loves_'. $post_id, $post_id, time()*20, '/');
-				
+
 				if( $loves == 0 ) { $suffix = $suffix_text_zero; }
 				elseif( $loves == 1 ) { $suffix = $suffix_text_one; }
 				else { $suffix = $suffix_text_more; }
-				
+
 				return '<span class="cce-loveit-count">'. $loves .'</span><span class="cce-loveit-suffix">'. $suffix .'</span>';
 				break;
-		
+
 		}
 	}
-	
+
 	function shortcode( $atts ) {
 		extract( shortcode_atts( array(
 		), $atts ) );
-		
+
 		return $this->do_likes();
 	}
-	
+
 	function do_likes($classes = null) {
 		global $post;
 		$classes = $classes ? ' ' . $classes : '';
 
         $cce_options = get_option('cce_options');
-		
+
 		$output = $this->cce_love_this($post->ID, $cce_options['suffix_text_zero'], $cce_options['suffix_text_one'], $cce_options['suffix_text_more']);
-  
+
   		$class = 'cce-loveit';
   		$title = __('Love this', 'cc-essentials');
   		$prefix_text = $cce_options['prefix_text'];
@@ -176,12 +176,12 @@ class CCELoveIt {
 			$class = 'cce-loveit loved';
 			$title = __('You already loved this!', 'cc-essentials');
 		}
-		
+
 		$prefix_span = ( '' != $prefix_text ) ? '<span class="cce-loveit-prefix">' . $prefix_text . '</span>' : false;
-		
+
 		return '<span class="cce-loveit-wrapper' . $classes . '">' . $prefix_span . '<a href="#" class="'. $class .'" id="cce-loveit-'. $post->ID .'" title="'. $title .'">'. $output .'</a></span>';
 	}
-	
+
 }
 
 $GLOBALS['cce_loveit'] = new CCELoveIt();
@@ -192,7 +192,7 @@ $GLOBALS['cce_loveit'] = new CCELoveIt();
 function cce_loveit()
 {
 	global $cce_loveit;
-    echo $cce_loveit->do_likes(); 
+    echo $cce_loveit->do_likes();
 }
 
 /**
@@ -202,7 +202,7 @@ function cce_loveit()
 class CCELoveIt_Widget extends WP_Widget {
 
 	function __construct() {
-		parent::__construct( 'cce_loveit_widget', __('CC - Most Loved Posts', 'cc-essentials'), array( 'description' => __('Displays most loved posts in descending order.', 'cc-essentials') ) );
+		parent::__construct( 'cce_loveit_widget', __('&#x95; [CC] Most Loved Posts', 'cc-essentials'), array( 'description' => __('Displays most loved posts in descending order. Installed by &#x2018;CC Essentials&#x2019; plugin.', 'cc-essentials') ) );
 	}
 
 	function widget( $args, $instance ) {
@@ -231,7 +231,7 @@ class CCELoveIt_Widget extends WP_Widget {
 		$loved_posts = get_posts($loved_posts_args);
 
 		echo '<ul class="cce_most_loved_posts">';
-		
+
 		global $cce_options;
 		foreach( $loved_posts as $loved_post ) {
 			$count_output = '';
@@ -281,22 +281,22 @@ class CCELoveIt_Widget extends WP_Widget {
 		?>
 
 		<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'cc-essentials'); ?></label> 
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'cc-essentials'); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('description'); ?>"><?php _e('Description:', 'cc-essentials'); ?></label> 
+			<label for="<?php echo $this->get_field_id('description'); ?>"><?php _e('Description:', 'cc-essentials'); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id('description'); ?>" name="<?php echo $this->get_field_name('description'); ?>" type="text" value="<?php echo $description; ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('posts'); ?>"><?php _e('Posts:', 'cc-essentials'); ?></label> 
+			<label for="<?php echo $this->get_field_id('posts'); ?>"><?php _e('Posts:', 'cc-essentials'); ?></label>
 			<input id="<?php echo $this->get_field_id('posts'); ?>" name="<?php echo $this->get_field_name('posts'); ?>" type="text" value="<?php echo $posts; ?>" size="3" />
 		</p>
 		<p>
 			<input id="<?php echo $this->get_field_id('display_count'); ?>" name="<?php echo $this->get_field_name('display_count'); ?>" type="checkbox" value="1" <?php checked( $display_count ); ?>>
 			<label for="<?php echo $this->get_field_id('display_count'); ?>"><?php _e('Display counts', 'cc-essentials'); ?></label>
 		</p>
-		
+
 		<p>
 			<input id="<?php echo $this->get_field_id('display_suffix_prefix'); ?>" name="<?php echo $this->get_field_name('display_suffix_prefix'); ?>" type="checkbox" value="1" <?php checked( $display_suffix_prefix ); ?>>
 			<label for="<?php echo $this->get_field_id('display_suffix_prefix'); ?>"><?php echo sprintf(__('Display suffix & prefix %1$sEdit%2$s', 'cc-essentials'), '<small>(<a href="admin.php?page=cce">', '</a>)</small>' ); ?></label>
